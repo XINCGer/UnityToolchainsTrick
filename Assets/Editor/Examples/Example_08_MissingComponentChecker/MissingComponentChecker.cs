1f0fb17f6e284154c955730406fd021f68d7bd86
马三小伙儿 <1790617178@qq.com>
2021-03-27 21:57:38 +0800

    MissingComponentsChecker添加

---
+++ Assets/Editor/Examples/Example_08_MissingComponentChecker/MissingComponentChecker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MissingComponentChecker
+{
+    private const string PATH = "";
+
+    [MenuItem("Tools/MissingComponentCheck",priority = 8)]
+    private static void MissingComponentCheck()
+    {
+        var guids = AssetDatabase.FindAssets("t:GameObject", new[] {PATH});
+        var length = guids.Length;
+        var index = 1;
+        foreach (var guid in guids)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            var cmps = prefab.GetComponentsInChildren<Component>(true);
+            for (int i = 0; i < cmps.Length; i++)
+            {
+                if (null == cmps[i])
+                {
+                    Debug.LogError($"prefab:{prefab.name}has missing compoents!path is:{path}");
+                }
+            }
+        }
+    }
+}
