ece784ea0e2a4ea6eb3682e259b73e67722bd40d
马三小伙儿 <1790617178@qq.com>
2021-03-31 19:36:44 +0800

    增加Example16_CreateMoreSceneView

---
+++ Assets/Editor/Examples/Example_16_MoreSceneView/CreateMoreSceneView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public static class CreateMoreSceneView
+    {
+        [MenuItem("Tools/CreateMoreSceneView", priority = 16)]
+        private static void PopUp()
+        {
+            var _window = ScriptableObject.CreateInstance<SceneView>();
+            _window.Init();
+            _window.Show();
+        }
+
+        private static void Init(this SceneView sceneView)
+        {
+            if (null != sceneView)
+            {
+                Debug.Log("SceneView Init!");
+            }
+        }
+    }
+}
