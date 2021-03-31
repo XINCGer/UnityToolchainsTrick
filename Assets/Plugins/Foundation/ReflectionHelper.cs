6bb791958ec23a7b8c6faefc04702025da4119ae
马三小伙儿 <1790617178@qq.com>
2021-03-31 22:36:19 +0800

    完善ReflectionHelper

--- Assets/Plugins/Foundation/ReflectionHelper.cs
+++ Assets/Plugins/Foundation/ReflectionHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace ToolKits
@@ -9,13 +10,30 @@ namespace ToolKits
     public class ReflectionHelper
     {
         public ReflectAnimator Animator;
+        public RefHandleUtility HandleUtility;
 
-        public ReflectionHelper()
+        private static ReflectionHelper _instance;
+
+        public static ReflectionHelper Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    _instance = new ReflectionHelper();
+                }
+
+                return _instance;
+            }
+        }
+
+        private ReflectionHelper()
         {
             Animator = new ReflectAnimator();
+            HandleUtility = new RefHandleUtility();
         }
     }
-    
+
     public class ReflectAnimator
     {
         private Type _type;
@@ -28,14 +46,36 @@ namespace ToolKits
             _bodyPositionInternal =
                 _type.GetProperty("bodyPositionInternal", BindingFlags.NonPublic | BindingFlags.Instance);
         }
-        
+
         public Vector3 GetBodyPosition(Animator animator)
         {
             if (null != _bodyPositionInternal)
             {
-                return (Vector3)_bodyPositionInternal.GetValue(animator, null);
+                return (Vector3) _bodyPositionInternal.GetValue(animator, null);
             }
+
             return Vector3.zero;
+        }
+    }
+
+    public class RefHandleUtility
+    {
+        private Type _type;
+
+        private MethodInfo _applyWireMaterial;
+
+        public RefHandleUtility()
+        {
+            _type = typeof(HandleUtility);
+            _applyWireMaterial = _type.GetMethod("ApplyWireMaterial", BindingFlags.NonPublic | BindingFlags.Static);
+        }
+
+        public void ApplyWireMaterial()
+        {
+            if (null != _applyWireMaterial)
+            {
+                _applyWireMaterial.Invoke(null, null);
+            }
         }
     }
 }
