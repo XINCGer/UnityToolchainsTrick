57e2d0418ae15bf67eeb840eddd4640c4ea2cc34
马三小伙儿 <1790617178@qq.com>
2021-04-01 21:23:21 +0800

    增加Example_14_Excel共享读取锁问题

---
+++ Assets/Editor/Examples/Example_14_ExcelShareRead/Example_14_ExcelShareRead.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using OfficeOpenXml;
+using UnityEditor;
+using UnityEngine;
+
+public class Example_14_ExcelShareRead
+{
+    private const string PATH = "Excels/Example.xlsx";
+
+    [MenuItem("Tools/Excel/NormalRead",priority = 14)]
+    private static void NormalRead()
+    {
+        using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.Read)))
+        {
+            var workbook = excelPackage.Workbook.Worksheets[1];
+            for (int i = 1; i <= workbook.Dimension.End.Row; i++)
+            {
+                var key = workbook.GetValue(i, 1);
+                Debug.Log($"Key: {key}");
+                for (int j = 2; j <= workbook.Dimension.End.Column; j++)
+                {
+                    var value = workbook.GetValue(i, j) as string;
+                    Debug.Log($"value{j} : {value}");
+                }
+            }
+        }
+    }
+
+    [MenuItem("Tools/Excel/ShareRead",priority = 14)]
+    private static void ShareRead()
+    {
+        using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.ReadWrite)))
+        {
+            var workbook = excelPackage.Workbook.Worksheets[1];
+            for (int i = 1; i <= workbook.Dimension.End.Row; i++)
+            {
+                var key = workbook.GetValue(i, 1);
+                Debug.Log($"Key: {key}");
+                for (int j = 2; j <= workbook.Dimension.End.Column; j++)
+                {
+                    var value = workbook.GetValue(i, j) as string;
+                    Debug.Log($"value{j} : {value}");
+                }
+            }
+        }
+    }
+}
