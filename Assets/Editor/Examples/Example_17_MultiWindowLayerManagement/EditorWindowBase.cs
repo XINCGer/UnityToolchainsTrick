935081d3e73a4793755a545852830381c3ada1ea
马三小伙儿 <1790617178@qq.com>
2021-04-01 22:30:25 +0800

    增加Example_17_MultiWindowLayerManagement

---
+++ Assets/Editor/Examples/Example_17_MultiWindowLayerManagement/EditorWindowBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    /// <summary>
+    /// 编辑器窗口基类
+    /// </summary>
+    public class EditorWindowBase : EditorWindow
+    {
+        /// <summary>
+        /// 界面层级管理，根据界面优先级访问界面焦点
+        /// </summary>
+        public int Priority { get; set; }
+
+        private void OnFocus()
+        {
+            //重写OnFocus方法，让EditorWindowMgr去自动排序汇聚焦点
+            EditorWindowMgr.FoucusWindow();
+        }
+    }
+}
+
