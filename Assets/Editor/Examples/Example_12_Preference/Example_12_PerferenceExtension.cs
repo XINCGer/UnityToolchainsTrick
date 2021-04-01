955ad608f2377fec606f3ea5bcfcb2808270479f
马三小伙儿 <1790617178@qq.com>
2021-04-01 22:22:19 +0800

    增加命名空间

--- Assets/Editor/Examples/Example_12_Preference/Example_12_PerferenceExtension.cs
+++ Assets/Editor/Examples/Example_12_Preference/Example_12_PerferenceExtension.cs
@@ -3,46 +3,50 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Example_12_PerferenceExtension
+namespace ToolKits
 {
-    private static bool loaded = false;
-    private static string context = "";
-    private const string KEY = "__ToolChainsTrick_Context__";
+    public class Example_12_PerferenceExtension
+    {
+        private static bool loaded = false;
+        private static string context = "";
+        private const string KEY = "__ToolChainsTrick_Context__";
     
 #if UNITY_2019_1_OR_NEWER
-    [SettingsProvider]
-    private static SettingsProvider ToolChainsTrickSetting()
-    {
-        var provider = new SettingsProvider("Preferences/ToolChainsTrick", SettingsScope.User)
+        [SettingsProvider]
+        private static SettingsProvider ToolChainsTrickSetting()
         {
-            guiHandler = (string key) =>
+            var provider = new SettingsProvider("Preferences/ToolChainsTrick", SettingsScope.User)
             {
-                PreferenceGUI();
-            },
-            keywords = new string[]{"Tool","Chains","Trick"},
-        };
-        return provider;
-    }
-    #else
+                guiHandler = (string key) =>
+                {
+                    PreferenceGUI();
+                },
+                keywords = new string[]{"Tool","Chains","Trick"},
+            };
+            return provider;
+        }
+#else
     [PreferenceItem( "ToolChainsTrick" )]
 #endif
-    private static void PreferenceGUI()
-    {
-        if (!loaded)
+        private static void PreferenceGUI()
         {
-            Load();
+            if (!loaded)
+            {
+                Load();
+            }
+            EditorGUI.BeginChangeCheck();
+            context = EditorGUILayout.TextField(new GUIContent("文本配置"),context);
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorPrefs.SetString(KEY,context);
+            }
         }
-        EditorGUI.BeginChangeCheck();
-        context = EditorGUILayout.TextField(new GUIContent("文本配置"),context);
-        if (EditorGUI.EndChangeCheck())
-        {
-            EditorPrefs.SetString(KEY,context);
-        }
-    }
 
-    private static void Load()
-    {
-        context = EditorPrefs.GetString(KEY, "");
-        loaded = true;
+        private static void Load()
+        {
+            context = EditorPrefs.GetString(KEY, "");
+            loaded = true;
+        }
     }
 }
+
--- Assets/Editor/Examples/Example_14_ExcelShareRead/Example_14_ExcelShareRead.cs
+++ Assets/Editor/Examples/Example_14_ExcelShareRead/Example_14_ExcelShareRead.cs
@@ -5,45 +5,49 @@ using OfficeOpenXml;
 using UnityEditor;
 using UnityEngine;
 
-public class Example_14_ExcelShareRead
+namespace ToolKits
 {
-    private const string PATH = "Excels/Example.xlsx";
-
-    [MenuItem("Tools/Excel/NormalRead",priority = 14)]
-    private static void NormalRead()
+    public class Example_14_ExcelShareRead
     {
-        using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.Read)))
+        private const string PATH = "Excels/Example.xlsx";
+
+        [MenuItem("Tools/Excel/NormalRead",priority = 14)]
+        private static void NormalRead()
         {
-            var workbook = excelPackage.Workbook.Worksheets[1];
-            for (int i = 1; i <= workbook.Dimension.End.Row; i++)
+            using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.Read)))
             {
-                var key = workbook.GetValue(i, 1);
-                Debug.Log($"Key: {key}");
-                for (int j = 2; j <= workbook.Dimension.End.Column; j++)
+                var workbook = excelPackage.Workbook.Worksheets[1];
+                for (int i = 1; i <= workbook.Dimension.End.Row; i++)
                 {
-                    var value = workbook.GetValue(i, j) as string;
-                    Debug.Log($"value{j} : {value}");
+                    var key = workbook.GetValue(i, 1);
+                    Debug.Log($"Key: {key}");
+                    for (int j = 2; j <= workbook.Dimension.End.Column; j++)
+                    {
+                        var value = workbook.GetValue(i, j) as string;
+                        Debug.Log($"value{j} : {value}");
+                    }
+                }
+            }
+        }
+
+        [MenuItem("Tools/Excel/ShareRead",priority = 14)]
+        private static void ShareRead()
+        {
+            using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.ReadWrite)))
+            {
+                var workbook = excelPackage.Workbook.Worksheets[1];
+                for (int i = 1; i <= workbook.Dimension.End.Row; i++)
+                {
+                    var key = workbook.GetValue(i, 1);
+                    Debug.Log($"Key: {key}");
+                    for (int j = 2; j <= workbook.Dimension.End.Column; j++)
+                    {
+                        var value = workbook.GetValue(i, j) as string;
+                        Debug.Log($"value{j} : {value}");
+                    }
                 }
             }
         }
     }
 
-    [MenuItem("Tools/Excel/ShareRead",priority = 14)]
-    private static void ShareRead()
-    {
-        using (ExcelPackage excelPackage = new ExcelPackage(new FileStream(PATH,FileMode.Open,FileAccess.Read,FileShare.ReadWrite)))
-        {
-            var workbook = excelPackage.Workbook.Worksheets[1];
-            for (int i = 1; i <= workbook.Dimension.End.Row; i++)
-            {
-                var key = workbook.GetValue(i, 1);
-                Debug.Log($"Key: {key}");
-                for (int j = 2; j <= workbook.Dimension.End.Column; j++)
-                {
-                    var value = workbook.GetValue(i, j) as string;
-                    Debug.Log($"value{j} : {value}");
-                }
-            }
-        }
-    }
 }
