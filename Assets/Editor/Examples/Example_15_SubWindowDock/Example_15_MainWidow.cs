3fa59d30305bf8b0ceb84dc294a31eeb65efd828
马三小伙儿 <1790617178@qq.com>
2021-04-03 22:20:51 +0800

    WindowDock提交主窗口代码

--- Assets/Editor/Examples/Example_15_SubWindowDock/Example_15_MainWidow.cs
+++ Assets/Editor/Examples/Example_15_SubWindowDock/Example_15_MainWidow.cs
@@ -1,11 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace ToolKits
 {
-    public class Example_15_MainWidow 
+    public class Example_15_MainWidow :EditorWindow
     {
-        
+        private static Example_15_MainWidow _window;
+        private static readonly Vector2 MIN_SIZE = new Vector2(400,300);
+        private const string KEY = "__EXAPLE_15_DOCK_LAYOUT_";
+        private bool isLayoutByCode = true;
+
+        [MenuItem("Tools/SubWindowDock",priority = 15)]
+        private static void PopUp()
+        {
+            _window = GetWindow<Example_15_MainWidow>("主界面");
+            _window.minSize = MIN_SIZE;
+            _window.Init();
+            _window.Show();
+        }
+
+        private void Init()
+        {
+            isLayoutByCode = EditorPrefs.GetBool(KEY, true);
+        }
+
+        private void OnGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            isLayoutByCode = GUILayout.Toggle(isLayoutByCode, "是否通过代码实现布局");
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorPrefs.SetBool(KEY,isLayoutByCode);
+            }
+        }
     }   
 }
