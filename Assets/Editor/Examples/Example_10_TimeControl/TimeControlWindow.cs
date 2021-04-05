51835becd64f33af8b6d699e74d9d6f030b6faec
马三小伙儿 <1790617178@qq.com>
2021-04-05 16:14:43 +0800

    添加example_28_SceneViewLock

---
+++ Assets/Editor/Examples/Example_10_TimeControl/TimeControlWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class TimeControlWindow : EditorWindow
+    {
+        private static TimeControlWindow _window;
+        private static readonly Vector2 MIN_SIZE = new Vector2(400, 300);
+        private TimeControl _timeControl;
+        private static readonly Rect TIME_RECR = new Rect(20, 20, 300, 50);
+
+        [MenuItem("Tools/TimeControlWindow", priority = 10)]
+        private static void PopUp()
+        {
+            _window = GetWindow<TimeControlWindow>("TimeControlWindow");
+            _window.minSize = _window.maxSize = MIN_SIZE;
+            _window.Init();
+            _window.Show();
+        }
+
+        private void Init()
+        {
+            _timeControl = new TimeControl();
+        }
+
+        private void OnGUI()
+        {
+            _timeControl.DoTimeControl(TIME_RECR);
+            _timeControl.Update();
+            Repaint();
+        }
+    }
+}
---
+++ Assets/Editor/Examples/Example_28_SceneViewLock/SceneViewLock.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class SceneViewLock
+    {
+        [MenuItem("Tools/SceneViewLock/LockDefault", priority = 28)]
+        private static void LockDefault()
+        {
+            var layer = LayerMask.NameToLayer("Default");
+            LockLayer(layer);
+        }
+
+        [MenuItem("Tools/SceneViewLock/UnLockDefault", priority = 28)]
+        private static void UnLocakDefault()
+        {
+            var layer = LayerMask.NameToLayer("Default");
+            UnLockLayer(layer);
+        }
+
+        /// <summary>
+        /// 设置锁定
+        /// </summary>
+        public static void LockLayer(int layer)
+        {
+            Tools.lockedLayers |= 1 << layer;
+        }
+
+        /// <summary>
+        /// 取消锁定
+        /// </summary>
+        public static void UnLockLayer(int layer)
+        {
+            Tools.lockedLayers &= ~(1 << layer);
+        }
+
+        /// <summary>
+        /// 切换锁定
+        /// </summary>
+        public static void SwichLockLayer(int layer)
+        {
+            Tools.lockedLayers ^= 1 << layer;
+        }
+
+        /// <summary>
+        /// 判断是否锁定
+        /// </summary>
+        public static bool IsLayerLocked(int layer)
+        {
+            return (Tools.lockedLayers & 1 << layer) == 1 << layer;
+        }
+    }
+}
