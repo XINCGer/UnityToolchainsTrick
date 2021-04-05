bd04996e598177211bac36e9be255da828e7f7d4
马三小伙儿 <1790617178@qq.com>
2021-04-05 15:33:51 +0800

    添加Example_26_预制体的修改与保存

---
+++ Assets/Editor/Examples/Example_26_PrefabModify/PrefabModify.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class PrefabModify
+    {
+        private const string PrefabPath = "Assets/GameAssets/Prefabs/Cube.prefab";
+
+        [MenuItem("Tools/PrefabModify/AddComponentAndSave", priority = 26)]
+        private static void AddComponentAndSave()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabPath);
+            var gameobject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (null == gameobject.GetComponent<ComponentA>())
+            {
+                gameobject.AddComponent<ComponentA>();
+            }
+            
+            PrefabUtility.SaveAsPrefabAssetAndConnect(gameobject, PrefabPath,InteractionMode.AutomatedAction);
+            GameObject.DestroyImmediate(gameobject);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        [MenuItem("Tools/PrefabModify/DelComponentAndSave", priority = 26)]
+        private static void DelComponentAndSave()
+        {
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(PrefabPath);
+            var gameobject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            var cmps = gameobject.GetComponents<ComponentA>();
+            foreach (var item in cmps)
+            {
+                Object.DestroyImmediate(item);
+            }
+
+            PrefabUtility.SaveAsPrefabAssetAndConnect(gameobject, PrefabPath,InteractionMode.AutomatedAction);
+            GameObject.DestroyImmediate(gameobject);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
