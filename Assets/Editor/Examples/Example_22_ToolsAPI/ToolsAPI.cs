748e79222cb659ad9e69dc8f3f988aa782bd2760
马三小伙儿 <1790617178@qq.com>
2021-04-05 14:47:16 +0800

    添加Example_22_ToolsAPI

---
+++ Assets/Editor/Examples/Example_22_ToolsAPI/ToolsAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class ToolsAPI : EditorWindow
+    {
+        private static ViewTool _viewTool;
+
+        [MenuItem("Tools/ToolsAPI", priority = 22)]
+        private static void PopUp()
+        {
+            var _window = GetWindow<ToolsAPI>("ToolsAPI");
+            SceneView.duringSceneGui -= OnSceneViewGUI;
+            SceneView.duringSceneGui += OnSceneViewGUI;
+            _window.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Tools.current"))
+            {
+                Debug.Log("Tools.current:" + Tools.current);
+            }
+        }
+
+        private static void OnSceneViewGUI(SceneView view)
+        {
+            if (Tools.viewTool != _viewTool)
+            {
+                Debug.Log("Tools.viewTool:" + Tools.viewTool);
+                _viewTool = Tools.viewTool;
+            }
+        }
+    }
+}
