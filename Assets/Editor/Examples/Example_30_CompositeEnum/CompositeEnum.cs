b577f42de44fb44301a6013be74a9d8a1dea982b
马三小伙儿 <1790617178@qq.com>
2021-04-06 21:44:37 +0800

    添加example_30_CompositeEnum

---
+++ Assets/Editor/Examples/Example_30_CompositeEnum/CompositeEnum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    [System.Flags]
+    public enum CompositeEnum
+    {
+        Red = 1 << 1,
+        Blue = 1 << 2,
+        Yellow = 1 << 3,
+        All = Red | Blue | Yellow,
+    }
+
+    public class CompositeEnumWindow : EditorWindow
+    {
+        private static CompositeEnumWindow _window;
+        private static readonly Vector2 MIN_SIE = new Vector2(400, 300);
+
+        private CompositeEnum _compositeEnum;
+
+        [MenuItem("Tools/复合枚举", priority = 30)]
+        private static void PopUp()
+        {
+            _window = GetWindow<CompositeEnumWindow>("复合枚举");
+            _window.minSize = MIN_SIE;
+            _window.Show();
+        }
+
+        private void OnGUI()
+        {
+            _compositeEnum = (CompositeEnum) EditorGUILayout.EnumFlagsField("复合枚举", _compositeEnum);
+            if (GUILayout.Button("是否包含Red"))
+            {
+                var contain = (_compositeEnum & CompositeEnum.Red) > 0;
+                Debug.Log($"是否包含Red？{contain}");
+            }
+        }
+    }
+}
