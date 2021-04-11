b5ba6ee49481d90ee673e67481221f6a14818c60
JingFengJi <961451518@qq.com>
2021-04-11 16:39:59 +0800

    案例36：开发者模式

---
+++ Assets/Editor/Examples/Example_36_DeveloperMode/DeveloperMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DeveloperMode : Editor
+{
+    [MenuItem("Tools/DeveloperMode/EnableDeveloperMode", priority = 36)]
+    private static void Enable()
+    {
+        UnityEditor.EditorPrefs.SetBool("DeveloperMode", true);
+    }
+    
+    [MenuItem("Tools/DeveloperMode/DisableDeveloperMode", priority = 36)]
+    private static void Disable()
+    {
+        UnityEditor.EditorPrefs.SetBool("DeveloperMode", false);
+    }
+}
