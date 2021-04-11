49dca478ceca199dc3729dc2835d46f62f51b0e7
JingFengJi <961451518@qq.com>
2021-04-11 22:37:21 +0800

    案例38：移除命名空间

--- Assets/Editor/Examples/Example_38_VideoEditorWindow/VideoWindow.cs
+++ Assets/Editor/Examples/Example_38_VideoEditorWindow/VideoWindow.cs
@@ -5,41 +5,39 @@ using UnityEngine;
 using UnityEngine.Video;
 using Object = System.Object;
 
-namespace ToolKits.Example_37_WebViewEditorWindow
+public class VideoWindow : EditorWindow
 {
-    public class VideoWindow : EditorWindow
+    private Object m_PreviewID;
+    private VideoClip m_PlayingClip;
+
+    [MenuItem("Tools/VideoEditorWindow", priority = 38)]
+    private static void ShowWindow()
     {
-        private Object m_PreviewID;
-        private VideoClip m_PlayingClip;
-        
-        [MenuItem("Tools/VideoEditorWindow", priority = 38)]
-        private static void ShowWindow()
+        var window = GetWindow<VideoWindow>();
+        window.titleContent = new GUIContent("VideoWindow");
+
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        m_PlayingClip = AssetDatabase.LoadAssetAtPath<VideoClip>("Assets/Editor/Examples/Example_38_VideoEditorWindow/Test.MP4");
+        m_PreviewID = VideoUtil.PlayPreview(m_PlayingClip);
+    }
+
+    private void OnDestroy()
+    {
+        VideoUtil.StopPreview(m_PreviewID);
+    }
+
+    private void OnGUI()
+    {
+        Texture image = VideoUtil.GetPreviewTexture(m_PreviewID);
+        if (image != null)
         {
-            var window = GetWindow<VideoWindow>();
-            window.titleContent = new GUIContent("VideoWindow");
-            
-            window.Show();
+            EditorGUI.DrawTextureTransparent(new Rect(0, 0, image.width, image.height), image, ScaleMode.ScaleToFit);
         }
 
-        private void OnEnable()
-        {
-            m_PlayingClip = AssetDatabase.LoadAssetAtPath<VideoClip>("Assets/Editor/Examples/Example_38_VideoEditorWindow/Test.MP4");
-            m_PreviewID = VideoUtil.PlayPreview(m_PlayingClip);
-        }
-
-        private void OnDestroy()
-        {
-            VideoUtil.StopPreview(m_PreviewID);
-        }
-
-        private void OnGUI()
-        {
-            Texture image = VideoUtil.GetPreviewTexture(m_PreviewID);
-            if (image != null)
-            {
-                EditorGUI.DrawTextureTransparent(new Rect(0,0,image.width, image.height), image, ScaleMode.ScaleToFit);
-            }
-            Repaint();
-        }
+        Repaint();
     }
 }
