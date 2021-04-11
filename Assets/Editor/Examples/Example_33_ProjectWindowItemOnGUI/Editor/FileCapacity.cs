f533659cae83b42c8b684b38d8080bdf1123550d
JingFengJi <961451518@qq.com>
2021-04-11 14:23:50 +0800

    案例33：ProjectWindow拓展，显示文件大小和拓展名

---
+++ Assets/Editor/Examples/Example_33_ProjectWindowItemOnGUI/Editor/FileCapacity.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public static class FileCapacity
+{
+    private const string REMOVE_STR = "Assets";
+    private const string FILESIZE = "FileSize";
+
+    private static readonly int mRemoveCount = REMOVE_STR.Length;
+    private static readonly Color mColor = new Color(0.635f, 0.635f, 0.635f, 1);
+    private static Dictionary<string, string> DirSizeDictionary = new Dictionary<string, string>();
+    private static List<string> DirList = new List<string>();
+
+    public static bool FileSizeEnable
+    {
+        get { return EditorPrefs.GetBool(FILESIZE, true); }
+        set { EditorPrefs.SetBool(FILESIZE, value); }
+    }
+    
+    [MenuItem("Tools/FileSize/OpenPlaySize")]
+    private static void OpenPlaySize()
+    {
+        FileSizeEnable = true;
+        GetPropjectDirs();
+    }
+
+    [MenuItem("Tools/FileSize/ClosePlaySize")]
+    private static void ClosePlaySize()
+    {
+        FileSizeEnable = false;
+        Init();
+    }
+
+    [InitializeOnLoadMethod]
+    private static void InitializeOnLoadMethod()
+    {
+        Init();
+        EditorApplication.projectChanged += GetPropjectDirs;
+        EditorApplication.projectWindowItemOnGUI += OnGUI;
+    }
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    private static void OnScriptsReloaded()
+    {
+        GetPropjectDirs();
+    }
+
+    private static void GetPropjectDirs()
+    {
+        Init();
+        if (FileSizeEnable == false) return;
+        GetAllDirecotries(Application.dataPath);
+        foreach (string path in DirList)
+        {
+            string newPath = path.Replace("\\", "/");
+            DirSizeDictionary.Add(newPath, GetFormatSizeString((int)GetDirectoriesSize(path)));
+        }
+    }
+    private static void Init()
+    {
+        DirSizeDictionary.Clear();
+        DirList.Clear();
+    }
+
+    private static void OnGUI(string guid, Rect selectionRect)
+    {
+        if (FileSizeEnable == false) return;
+        var dataPath = Application.dataPath;
+        var startIndex = dataPath.LastIndexOf(REMOVE_STR);
+        var dir = dataPath.Remove(startIndex, mRemoveCount);
+        var path = dir + AssetDatabase.GUIDToAssetPath(guid);
+        string text = null;
+        if (DirSizeDictionary.ContainsKey(path))
+        {
+            text = DirSizeDictionary[path];
+        }
+        else if (File.Exists(path))
+        {
+            var fileInfo = new FileInfo(path);
+            var fileSize = fileInfo.Length;
+            text = GetFormatSizeString((int)fileSize);
+        }
+        else
+        {
+            return;
+        }
+
+
+
+        var label = EditorStyles.label;
+        var content = new GUIContent(text);
+        var width = label.CalcSize(content).x + 10;
+
+        var pos = selectionRect;
+        pos.x = pos.xMax - width;
+        pos.width = width;
+        pos.yMin++;
+
+        var color = GUI.color;
+        GUI.color = mColor;
+        GUI.DrawTexture(pos, EditorGUIUtility.whiteTexture);
+        GUI.color = color;
+        GUI.Label(pos, text);
+    }
+
+    private static string GetFormatSizeString(int size)
+    {
+        return GetFormatSizeString(size, 1024);
+    }
+
+    private static string GetFormatSizeString(int size, int p)
+    {
+        return GetFormatSizeString(size, p, "#,##0.##");
+    }
+
+    private static string GetFormatSizeString(int size, int p, string specifier)
+    {
+        var suffix = new[] { "", "K", "M", "G", "T", "P", "E", "Z", "Y" };
+        int index = 0;
+
+        while (size >= p)
+        {
+            size /= p;
+            index++;
+        }
+
+        return string.Format(
+            "{0}{1}B",
+            size.ToString(specifier),
+            index < suffix.Length ? suffix[index] : "-"
+        );
+    }
+
+    private static void GetAllDirecotries(string dirPath)
+    {
+        if (Directory.Exists(dirPath) == false)
+        {
+            return;
+        }
+        DirList.Add(dirPath);
+        DirectoryInfo[] dirArray = new DirectoryInfo(dirPath).GetDirectories();
+        foreach (DirectoryInfo item in dirArray)
+        {
+            GetAllDirecotries(item.FullName);
+        }
+    }
+
+    private static long GetDirectoriesSize(string dirPath)
+    {
+        if (Directory.Exists(dirPath) == false)
+        {
+            return 0;
+        }
+
+        long size = 0;
+        DirectoryInfo dir = new DirectoryInfo(dirPath);
+        foreach (FileInfo info in dir.GetFiles())
+        {
+            size += info.Length;
+        }
+
+        DirectoryInfo[] dirBotton = dir.GetDirectories();
+        foreach (DirectoryInfo info in dirBotton)
+        {
+            size += GetDirectoriesSize(info.FullName);
+        }
+        return size;
+    }
+}
---
+++ Assets/Editor/Examples/Example_33_ProjectWindowItemOnGUI/Editor/ShowFileExtensions.cs
@@ -0,0 +1,150 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class ShowFileExtensions
+{
+    public static bool ShowFileExtensionsEnable
+    {
+        get { return EditorPrefs.GetBool("ShowFileExtensions", true); }
+        set { EditorPrefs.SetBool("ShowFileExtensions", value); }
+    }
+    
+    [MenuItem("Tools/ShowFileExtensions/OpenShowFileExtensions")]
+    private static void OpenPlaySize()
+    {
+        ShowFileExtensionsEnable = true;
+    }
+
+    [MenuItem("Tools/ShowFileExtensions/CloseShowFileExtensions")]
+    private static void ClosePlaySize()
+    {
+        ShowFileExtensionsEnable = false;
+    }
+    
+    static ShowFileExtensions()
+    {
+        EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGUI;
+    }
+    private static void ProjectWindowItemOnGUI(string guid, Rect rect)
+    {
+        if (ShowFileExtensionsEnable && Event.current.alt)
+        {
+            EditorWindow window = GetProjectWindow();            
+           string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            UnityEngine.Object obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
+
+            if (obj != null && AssetDatabase.IsMainAsset(obj) && !IsDirectory(obj))
+            {
+                if (showsBigIcons)
+                {
+                    // just draw it bold in upper left
+                    string extension = Path.GetExtension(assetPath);
+                    GUI.Label(rect, extension, EditorStyles.boldLabel);
+                }
+                else
+                {
+                    // we overpaint the filename here, does look a bit dirty and might need adjustment of the offset
+                    var labelRect = rect.Translate(new Vector2(19.5f, 0f));
+                    var fileName = Path.GetFileName(assetPath);
+                    GUI.Label(labelRect, fileName);
+                }
+            }
+
+            EditorApplication.RepaintProjectWindow();
+        }
+    }
+
+    // ================================================================================
+    //  getting infos about the project window
+    // --------------------------------------------------------------------------------
+
+    private static bool showsBigIcons
+    {
+        get
+        {
+            return isTwoColumnMode && listAreaGridSize > 16f;
+        }
+    }
+
+    private static bool isTwoColumnMode
+    {
+        get
+        {
+            var projectWindow = GetProjectWindow();
+
+            var projectWindowType = projectWindow.GetType();
+            var modeFieldInfo = projectWindowType.GetField("m_ViewMode", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            int mode = (int)modeFieldInfo.GetValue(projectWindow);
+
+            // 0 == ViewMode.OneColumn
+            // 1 == ViewMode.TwoColum
+
+            return mode == 1;
+        }
+    }
+
+    private static float listAreaGridSize
+    {
+        get
+        {
+            var projectWindow = GetProjectWindow();
+
+            var projectWindowType = projectWindow.GetType();
+            var propertyInfo = projectWindowType.GetProperty("listAreaGridSize", BindingFlags.Instance | BindingFlags.Public);
+            return (float)propertyInfo.GetValue(projectWindow, null);
+        }
+    }
+
+    /// <summary>
+    /// there's a chance here we get the wrong one when two project windows are open
+    /// </summary>
+    private static EditorWindow GetProjectWindow()
+    {
+        if (EditorWindow.focusedWindow != null && EditorWindow.focusedWindow.titleContent.text == "Project")
+        {
+            return EditorWindow.focusedWindow;
+        }
+
+        return GetExistingWindowByName("Project");
+    }
+
+    private static EditorWindow GetExistingWindowByName(string name)
+    {
+        EditorWindow[] windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+        foreach (var item in windows)
+        {
+            if (item.titleContent.text == name)
+            {
+                return item;
+            }
+        }
+
+        return default(EditorWindow);
+    }
+
+    // ================================================================================
+    //  utilities
+    // --------------------------------------------------------------------------------
+
+    private static Rect Translate(this Rect rect, Vector2 delta)
+    {
+        rect.x += delta.x;
+        rect.y += delta.y;
+
+        return rect;
+    }
+
+    private static bool IsDirectory(UnityEngine.Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        return obj is DefaultAsset && !AssetDatabase.IsForeignAsset(obj);
+    }
+}
