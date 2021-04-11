801a74f88a17b97197b54584c5a1566b313a1cf9
JingFengJi <961451518@qq.com>
2021-04-11 16:29:31 +0800

    案例35：修改接口参数类型

--- Assets/Editor/Examples/Example_35_SetObjectIcon/EditorGUIExtension.cs
+++ Assets/Editor/Examples/Example_35_SetObjectIcon/EditorGUIExtension.cs
@@ -10,10 +10,10 @@ namespace SetObjectIcon
         //sv_icon_name0 - sv_icon_name7
         //sv_icon_dot0_sml - sv_icon_dot15_sml
         //sv_icon_dot0_pix16_gizmo - sv_icon_dot15_pix16_gizmo
-        public static void SetIcon(GameObject g, string name)
+        public static void SetIcon(Object obj, string name)
         {
             Texture2D tex = EditorGUIUtility.IconContent(name).image as Texture2D;
-            object[] args = new object[] {g, tex};
+            object[] args = new object[] {obj, tex};
             typeof(EditorGUIUtility).InvokeMember("SetIconForObject", BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic, null, null, args);
         }
     }
