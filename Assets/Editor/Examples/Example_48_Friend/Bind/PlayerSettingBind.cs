5b25f5dfb66a7cf4789f4c6715a2286453058b6c
Icarus <1375400884@qq.com>
2021-05-21 21:57:35 +0800

    !A(Example) 48, 使用友元访问Unity内部API

---
+++ Assets/Editor/Examples/Example_48_Friend/Bind/PlayerSettingBind.cs
@@ -0,0 +1,14 @@
+//Create: Icarus
+//ヾ(•ω•`)o
+//2021-05-21 09:49
+//Unity.InternalAPIEngineBridge.006
+
+using UnityEditor;
+
+namespace UnityToolchinsTrick
+{
+    public static class PlayerSettingBind
+    {
+        public static char[] PlayerSettingDefineSplits => PlayerSettings.defineSplits;
+    }
+}
---
+++ Assets/Editor/Examples/Example_48_Friend/UseBind.cs
@@ -0,0 +1,17 @@
+//Create: Icarus
+//ヾ(•ω•`)o
+//2021-05-21 09:53
+//Unity.InternalAPIEngineBridge.006
+
+using UnityEngine;
+
+namespace UnityToolchinsTrick
+{
+    public class UseBind
+    {
+        void Use()
+        {
+            Debug.Log(PlayerSettingBind.PlayerSettingDefineSplits);
+        }
+    }
+}
