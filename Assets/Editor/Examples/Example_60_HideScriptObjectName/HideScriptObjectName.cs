1a435e5340df8164a5f5e35bd8bd9a5ee2ec573a
马三小伙儿 <1790617178@qq.com>
2021-07-04 22:01:30 +0800

    增加案例Example_60_HideScriptObjectName

---
+++ Assets/Editor/Examples/Example_60_HideScriptObjectName/HideScriptObjectName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToolKits
+{
+    [CreateAssetMenu(fileName = "HideScriptObjectName.asset",menuName = "CustomAssets/HideScriptObjectName")]
+    public class HideScriptObjectName : ScriptableObject
+    {
+        public string Name;
+        public int Age;
+        public string Readme = "隐藏ScriptableObject的脚本信息展示";
+    }
+}
+
---
+++ Assets/Editor/Examples/Example_60_HideScriptObjectName/HideScriptObjectNameInspector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    [CustomEditor(typeof(HideScriptObjectName), true)]
+    public class HideScriptObjectNameInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            serializedObject.Update();
+
+            SerializedProperty property = serializedObject.GetIterator();
+            bool expanded = true;
+            while (property.NextVisible(expanded))
+            {
+                expanded = false;
+                if (SkipField(property.propertyPath))
+                    continue;
+                EditorGUILayout.PropertyField(property, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            EditorGUI.EndChangeCheck();
+        }
+
+        public virtual void ApplyChanges()
+        {
+        }
+
+        static bool SkipField(string fieldName)
+        {
+            return fieldName == "m_Script";
+        }
+    }
+}
