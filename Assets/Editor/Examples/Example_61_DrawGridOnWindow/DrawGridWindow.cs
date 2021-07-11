5f404f8487e6bf626c8d9292f684ba082aaa47fc
马三小伙儿 <1790617178@qq.com>
2021-07-11 22:36:48 +0800

    添加案例61_DrawGridOnWindow

---
+++ Assets/Editor/Examples/Example_61_DrawGridOnWindow/DrawGridWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class DrawGridWindow : EditorWindow
+    {
+        private static DrawGridWindow _window;
+        private static Vector2 MIN_SIZE = new Vector2(400, 300);
+
+        private Color backgroundColor;
+        private Color gridColor;
+
+        [MenuItem("Tools/绘制网格的示例窗口", priority = 61)]
+        private static void PopUp()
+        {
+            _window = GetWindow<DrawGridWindow>("绘制网格的示例窗口");
+            _window.minSize = MIN_SIZE;
+            _window.Init();
+            _window.Show();
+        }
+
+        private void Init()
+        {
+            backgroundColor = new Color(0.4f, 0.4f, 0.4f);
+            gridColor = new Color(0.1f, 0.1f, 0.1f);
+        }
+
+        private void OnGUI()
+        {
+            DrawBackground();
+            DrawGrid(10, 0.2f);
+            DrawGrid(50, 0.4f);
+        }
+
+        private void DrawBackground()
+        {
+            EditorGUI.DrawRect(new Rect(0, 0, position.width, position.height), backgroundColor);
+        }
+
+        private void DrawGrid(float gridSpacing, float gridOpacity)
+        {
+            int widthDivs = Mathf.CeilToInt(position.width / gridSpacing);
+            int heightDivs = Mathf.CeilToInt(position.height / gridSpacing);
+            Handles.BeginGUI();
+            Handles.color = new Color(gridColor.r, gridColor.g, gridColor.b, gridOpacity);
+            var drag = Vector2.one;
+            var offset = Vector2.one;
+            offset += drag * 0.5f;
+            Vector3 newOffset = new Vector3(offset.x % gridSpacing, offset.y % gridSpacing, 0);
+            for (int i = 0; i < widthDivs; i++)
+            {
+                Handles.DrawLine(new Vector3(gridSpacing * i, -gridSpacing, 0) + newOffset,
+                    new Vector3(gridSpacing * i, position.height, 0f) + newOffset);
+            }
+
+            for (int j = 0; j < heightDivs; j++)
+            {
+                Handles.DrawLine(new Vector3(-gridSpacing, gridSpacing * j, 0) + newOffset,
+                    new Vector3(position.width, gridSpacing * j, 0f) + newOffset);
+            }
+
+            Handles.color = Color.white;
+            Handles.EndGUI();
+        }
+    }
+}
