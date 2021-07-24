905c6df928f2e7de9f49e0ce3d233f1ea4114e96
马三小伙儿 <1790617178@qq.com>
2021-07-24 22:25:14 +0800

    新添加支持GL绘制网格的方式

--- Assets/Editor/Examples/Example_61_DrawGridOnWindow/DrawGridWindow.cs
+++ Assets/Editor/Examples/Example_61_DrawGridOnWindow/DrawGridWindow.cs
@@ -16,6 +16,11 @@ namespace ToolKits
         private Vector2 drag;
         private Vector2 offset;
 
+        private Rect mGraphRect;
+        private Vector2 mGraphOffset = Vector2.zero;
+        private float mGraphZoom = 1f;
+        private Material mGridMaterial;
+
         [MenuItem("Tools/绘制网格的示例窗口", priority = 61)]
         private static void PopUp()
         {
@@ -29,14 +34,23 @@ namespace ToolKits
         {
             backgroundColor = new Color(0.4f, 0.4f, 0.4f);
             gridColor = new Color(0.1f, 0.1f, 0.1f);
+
+            if (mGridMaterial == null)
+            {
+                mGridMaterial = new Material(Shader.Find("Hidden/Behavior Designer/Grid"));
+                mGridMaterial.hideFlags = HideFlags.HideAndDontSave;
+                mGridMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+            }
         }
 
         private void OnGUI()
         {
+            SetUpSize();
             ProcessEvents(Event.current);
             DrawBackground();
-            DrawGrid(10, 0.2f);
-            DrawGrid(50, 0.4f);
+            // DrawGrid(10, 0.2f);
+            // DrawGrid(50, 0.4f);
+            DrawGrid();
             if (GUI.changed) Repaint();
         }
 
@@ -84,10 +98,65 @@ namespace ToolKits
             }
         }
 
+        private void SetUpSize()
+        {
+            mGraphRect = new Rect(Vector2.zero, new Vector2(position.width, position.height));
+        }
+
         private void OnDrag(Vector2 delta)
         {
             drag = delta;
             GUI.changed = true;
+        }
+
+        private void DrawGrid()
+        {
+            mGridMaterial.SetPass((!EditorGUIUtility.isProSkin) ? 1 : 0);
+            GL.PushMatrix();
+            GL.Begin(1);
+            DrawGridLines(10f * mGraphZoom,
+                new Vector2(mGraphOffset.x % 10f * mGraphZoom, mGraphOffset.y % 10f * mGraphZoom));
+            GL.End();
+            GL.PopMatrix();
+            mGridMaterial.SetPass((!EditorGUIUtility.isProSkin) ? 3 : 2);
+            GL.PushMatrix();
+            GL.Begin(1);
+            DrawGridLines(50f * mGraphZoom,
+                new Vector2(mGraphOffset.x % 50f * mGraphZoom, mGraphOffset.y % 50f * mGraphZoom));
+            GL.End();
+            GL.PopMatrix();
+        }
+
+        private void DrawGridLines(float gridSize, Vector2 offset)
+        {
+            float num = mGraphRect.x + offset.x;
+            if (offset.x < 0f)
+            {
+                num += gridSize;
+            }
+
+            for (float num2 = num; num2 < mGraphRect.x + mGraphRect.width; num2 += gridSize)
+            {
+                DrawLine(new Vector2(num2, mGraphRect.y)
+                    , new Vector2(num2, mGraphRect.y + mGraphRect.height));
+            }
+
+            float num3 = mGraphRect.y + offset.y;
+            if (offset.y < 0f)
+            {
+                num3 += gridSize;
+            }
+
+            for (float num4 = num3; num4 < mGraphRect.y + mGraphRect.height; num4 += gridSize)
+            {
+                DrawLine(new Vector2(mGraphRect.x, num4), new Vector2(mGraphRect.x + mGraphRect.width, num4));
+            }
+        }
+
+        private void DrawLine(Vector2 p1, Vector2 p2)
+        {
+            GL.Vertex(p1);
+            GL.Vertex(p2);
         }
     }
 }
