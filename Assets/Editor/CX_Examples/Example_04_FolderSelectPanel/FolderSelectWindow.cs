d9c089dafddea3163e8836bbbdc794d6c0f5cbdf
CX_Project <332147951@qq.com>
2021-11-09 15:28:13 +0800

    update FolderSelect

---
+++ Assets/Editor/CX_Examples/Example_04_FolderSelectPanel/FolderSelectWindow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace CX_Examples.Example_04
+{
+    public class FolderSelectWindow : EditorWindow
+    {
+        [SerializeField] private TreeViewState m_treeViewState;
+        private FolderSelectView m_TreeView;
+        
+        [MenuItem("CX_Tools/FolderSelect")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<FolderSelectWindow>();
+            window.Show();
+        }
+
+        private void OnEnable()
+        {
+            if (m_treeViewState == null)
+                m_treeViewState = new TreeViewState ();
+            m_TreeView = new FolderSelectView (m_treeViewState);
+            //DoTreeView();
+        }
+
+        private void OnGUI()
+        {
+            DoTreeView ();
+            if (GUILayout.Button("Export"))
+            {
+                ShowSelectedFolder();
+            }
+        }
+        
+        void DoTreeView ()
+        {
+            Rect rect = GUILayoutUtility.GetRect (0, 100000, 0, 100000);
+            m_TreeView.OnGUI(rect);
+        }
+
+        void ShowSelectedFolder()
+        {
+           var folders = m_TreeView.GetAllSelectFolderPath();
+           foreach (var folder in folders)
+           {
+               Debug.Log(folder);
+           }
+        }
+    }
+}
