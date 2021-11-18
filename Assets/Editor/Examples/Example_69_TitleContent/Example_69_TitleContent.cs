d0b37c219d81526830a692df1664da3d5a6ad03f
马三小伙儿 <1790617178@qq.com>
2021-11-18 19:50:38 +0800

    添加案例69 titlecontent

---
+++ Assets/Editor/Examples/Example_69_TitleContent/Example_69_TitleContent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ToolKits
+{
+    public class Example_69_TitleContent : EditorWindow
+    {
+        private static Example_69_TitleContent _window;
+        
+        [MenuItem("Tools/Example_69_TitleContent", priority = 69)]
+        private static void PopUp()
+        {
+            _window = GetWindow<Example_69_TitleContent>("TitleContent用法展示");
+            var titleContent = new GUIContent();
+            titleContent.image = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Editor/Textures/logo.png");
+            _window.titleContent = titleContent;
+            _window.Show();
+        }
+    }   
+}
