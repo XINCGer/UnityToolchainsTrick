df963b45f055217aea2bbea03f16ee7c6db2dcbe
CX_Project <332147951@qq.com>
2022-03-05 19:33:36 +0800

    update

---
+++ Assets/Scripts/CX_Example/Example_15_EditorFrameTool/Editor/EditorBoxEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Example_15_EditorFrameTool
+{
+    [CustomEditor(typeof(EditorBox))]
+    public class EditorBoxEditor : Editor
+    {
+        private void OnSceneGUI()
+        {
+            var editorBox = target as EditorBox;
+            Handles.DrawWireCube(editorBox.Center, editorBox.Size);
+            Handles.PositionHandle(editorBox.Center, editorBox.transform.rotation);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/CX_Example/Example_15_EditorFrameTool/EditorBox.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Example_15_EditorFrameTool
+{
+    public class EditorBox : MonoBehaviour
+    {
+        public Vector3 Center;
+        public Vector3 Size;
+    }
+}
