e1638d4a1a16066bafd7ad17efd381512f8d352d
XINCGer <1790617178@qq.com>
2022-05-13 00:34:48 +0800

    修正ObjectPreview Frame时长不正确的bug

--- Assets/Editor/Examples/Example_63_ObjectPreview/PreviewWindow.cs
+++ Assets/Editor/Examples/Example_63_ObjectPreview/PreviewWindow.cs
@@ -18,16 +18,21 @@ namespace CZToolKit.Examples
 
         private void OnEnable()
         {
-            UnityEngine.Object model = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>("Assets/Editor/Examples/Example_63_ObjectPreview/SF_Animal_Boar.fbx");
+            UnityEngine.Object model =
+                AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(
+                    "Assets/Editor/Examples/Example_63_ObjectPreview/SF_Animal_Boar.fbx");
             modelEditor = Editor.CreateEditor(model);
+            AnimationClip _clip = null;
 
-            foreach (var item in AssetDatabase.LoadAllAssetRepresentationsAtPath("Assets/Editor/Examples/Example_63_ObjectPreview/SF_Animal_Boar.fbx"))
+            foreach (var item in AssetDatabase.LoadAllAssetRepresentationsAtPath(
+                         "Assets/Editor/Examples/Example_63_ObjectPreview/SF_Animal_Boar.fbx"))
             {
                 switch (item)
                 {
                     case AnimationClip animationClip:
                         if (animEditor == null)
                             animEditor = Editor.CreateEditor(animationClip);
+                        _clip = animationClip;
                         continue;
                     case Mesh mesh:
                         if (meshEditor == null)
@@ -41,16 +46,34 @@ namespace CZToolKit.Examples
                         break;
                 }
             }
-            Type animationClipEditorType = Assembly.Load("UnityEditor.CoreModule").GetType("UnityEditor.AnimationClipEditor");
-            MethodInfo initMethod = animationClipEditorType.GetMethod("Init", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            Type animationClipEditorType =
+                Assembly.Load("UnityEditor.CoreModule").GetType("UnityEditor.AnimationClipEditor");
+            MethodInfo initMethod =
+                animationClipEditorType.GetMethod("Init", BindingFlags.Instance | BindingFlags.NonPublic);
             initMethod.Invoke(animEditor, null);
+            
+            var avatarPreviewFieldInfo =
+                animationClipEditorType.GetField("m_AvatarPreview", BindingFlags.Instance | BindingFlags.NonPublic);
+            var avatarPreviewValue = avatarPreviewFieldInfo.GetValue(animEditor);
+            
+            Type avatarPreviewType = Type.GetType("UnityEditor.AvatarPreview,UnityEditor.CoreModule");
+            var timeControlFieldInfo =
+                avatarPreviewType.GetField("timeControl", BindingFlags.Instance | BindingFlags.Public);
+            var timeControlValue = timeControlFieldInfo.GetValue(avatarPreviewValue);
+            
+            var timeControlType = Type.GetType("UnityEditor.TimeControl,UnityEditor.CoreModule");
+            var stopTimeFieldInfo = timeControlType.GetField("stopTime", BindingFlags.Instance | BindingFlags.Public);
+            stopTimeFieldInfo.SetValue(timeControlValue, _clip.length);
 
-
-
-            UnityEngine.Object texture = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>("Assets/Editor/Examples/Example_63_ObjectPreview/Wallpaper 5.png");
+            UnityEngine.Object texture =
+                AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(
+                    "Assets/Editor/Examples/Example_63_ObjectPreview/Wallpaper 5.png");
             textureEditor = Editor.CreateEditor(texture);
 
-            UnityEngine.Object audioClip = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>("Assets/Editor/Examples/Example_63_ObjectPreview/Click.wav");
+            UnityEngine.Object audioClip =
+                AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(
+                    "Assets/Editor/Examples/Example_63_ObjectPreview/Click.wav");
             audioEditor = Editor.CreateEditor(audioClip);
         }
 
